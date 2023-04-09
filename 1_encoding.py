f7477c8b77232994da3fc8689b82e6f63d10681c
Nhatziet <92376163+trananhnhatviet@users.noreply.github.com>
2023-04-09 11:24:13 +0700

    For Trainee

---
+++ 1_encoding.py
@@ -0,0 +1,11 @@
+#Chall này dựa vào kiến thức General và tạo acc Cryptohack
+
+from Crypto.Util.number import*
+from base64 import*
+flag='shark{????????????????????????????}'
+#Giữa các f_block sẽ có dấu '_'
+f_block_1 = 418547722601
+f_block_4 = 'YmlnSW50ZXJnZXI='
+f_block_3 = 626173653634
+f_block_2 = 'rohknomswkv'
+
