878f9af0ca7bb560a92f0d60a54915840ccd1f8c
Nhatziet <92376163+trananhnhatviet@users.noreply.github.com>
2023-04-09 12:12:30 +0700

    For Trainee

--- 4_Rabbit_On_Table_13.py
+++ 4_Rabbit_On_Table_13.py
@@ -1,3 +1,3 @@
-#Hoàng đế ở La Mã
+#Con số mà nhiều người coi là không tốt thường được bỏ qua trong các khu vực đếm số, vì nó được cho là mang lại điều không may và rủi ro.
 
 lst=[121, 114, 103, 95, 122, 114, 95, 113, 98, 106, 97, 95, 102, 121, 98, 106, 121, 108]
