f85702dc4e11e41f182c4d0f0b36307d5b4b8efa
Nhatziet <92376163+trananhnhatviet@users.noreply.github.com>
2023-04-09 22:58:56 +0700

    Update 8_Caesar_mutate.py

--- 8_Caesar_mutate.py
+++ 8_Caesar_mutate.py
@@ -1,3 +1,4 @@
 #Số nguyên tố đầu tiên là sao? Tui hông biết nó dùng để làm gì
+#Nếu giải được mã ciphertext thì sẽ có cả 'shark' 
 
 ciphertext = 'qe]ietklfZfR5RUbO_KNKWG`UZCGGDRXNQ;DN8LF5<9G17<05J'
